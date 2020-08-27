2d9aca4c74e1a33235f08d0e49c6ebf0bcdf5be1
it-web-life <info@it-web-life.com>
2020-08-27 14:11:14 +0900

    :sparkles: feat: throttleを実装

---
+++ complete_throttle/script/script.js
@@ -0,0 +1,20 @@
+$(function() {
+  // トップへ戻るボタン
+  $(window).scroll(
+    $.throttle(250, function() {
+      if ($(this).scrollTop() > 100) {
+        $('.backToTop').fadeIn();
+      } else {
+        $('.backToTop').fadeOut();
+      }
+    })
+  );
+
+  // トップへ戻るボタンのクリック
+  $('.backToTop').on('click', function() {
+    $('body,html').animate({
+      scrollTop:0
+    },500);
+    return false;
+  });
+});
