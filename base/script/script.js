9772bd956b4511373a09c68f27985556a75f2d8c
it-web-life <info@it-web-life.com>
2020-08-27 14:10:52 +0900

    :sparkles: feat: ベースとなるファイルを追加

---
+++ base/script/script.js
@@ -0,0 +1,18 @@
+$(function() {
+  // トップへ戻るボタンの表示制御
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.backToTop').fadeIn();
+    } else{
+      $('.backToTop').fadeOut();
+    }
+  });
+
+  // トップへ戻るボタンのクリック
+  $('.backToTop').on('click', function() {
+    $('body,html').animate({
+      scrollTop:0
+    },500);
+    return false;
+  });
+});
